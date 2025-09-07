27142094267aae38b2065c4a4ad4d6f19cfc9c12
Ka-atsu <tallaferkent775@gmail.com>
2025-09-07 14:22:08 +0800

    Added button

--- tessela_frontend/src/Components/Shopping_Page/Hero/HeroCrowdFund.js
+++ tessela_frontend/src/Components/Shopping_Page/Hero/HeroCrowdFund.js
@@ -1,18 +1,21 @@
-import './Hero.css';
+import { Link } from "react-router-dom";
+import "./Hero.css";
 
 const HeroCrowdFund = () => {
-    return (
-        <>
-            <div className='hero-overlay'></div>
-            <div className='hero'> 
-                <div>
-                    <h1>Hello Crowd Fund</h1>
-                    <h2>Connecting the World.</h2>
-                </div>
-            </div>
-
-        </>
-    )
+  return (
+    <>
+      <div className="hero-overlay"></div>
+      <div className="hero">
+        <div className="hero-content">
+          <h1>Hello Crowd Fund</h1>
+          <h2>Connecting the World.</h2>
+          <Link to="/donate"  className="marketplace-link">
+            Donate Now
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HeroCrowdFund;
