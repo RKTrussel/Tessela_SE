e18e285c5ef266784a8403b43a0248a3d5edb9b8
RKTrussel <129744087+RKTrussel@users.noreply.github.com>
2025-09-07 15:17:35 +0800

    Added details in Crowd Hero Page

--- tessela_frontend/src/Components/Shopping_Page/Hero/HeroCrowdFund.js
+++ tessela_frontend/src/Components/Shopping_Page/Hero/HeroCrowdFund.js
@@ -1,19 +1,32 @@
 import { Link } from "react-router-dom";
+import { Container, Row, Col } from 'react-bootstrap';
 import "./Hero.css";
+import PeopleWeaving from '../../../Assets/PeopleWeaving.jpeg';
 
 const HeroCrowdFund = () => {
   return (
     <>
-      <div className="hero-overlay"></div>
-      <div className="hero">
-        <div className="hero-content">
-          <h1>Hello Crowd Fund</h1>
-          <h2>Connecting the World.</h2>
-          <Link to="/donate"  className="marketplace-link">
-            Donate Now
-          </Link>
-        </div>
-      </div>
+      <div className='hero-overlay'></div>
+            <div className='hero'>
+                <Container className="h-100">
+                    <Row className="h-100 align-items-center">
+                        <Col className="hero-blog-left">
+                            <h2>Weave Hope with Every Gift</h2>
+                            <p>Your donation helps preserve indigenous traditions and uplift communities.</p>
+                            <Link to="/donate" className="marketplace-link mt-3">
+                                Donate Now
+                            </Link>
+                        </Col>
+                        <Col>
+                            <img 
+                                src={PeopleWeaving} 
+                                alt="Handcrafted market" 
+                                className="hero-blog-img"
+                            />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
     </>
   );
 };
